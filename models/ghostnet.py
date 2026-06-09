20520125a323fbd6e36f34b2acfa435e87fb7f10
xjkgis <905172622@qq.com>
2026-06-09 11:39:28 +0800

    Update ghostnet.py

--- models/ghostnet.py
+++ models/ghostnet.py
@@ -16,12 +16,6 @@ __all__ = ['ghost_net']
 
 
 def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    """
     if min_value is None:
         min_value = divisor
     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
@@ -100,8 +94,6 @@ class GhostModule(nn.Module):
 
 
 class GhostBottleneck(nn.Module):
-    """ Ghost bottleneck w/ optional SE"""
-
     def __init__(self, in_chs, mid_chs, out_chs, dw_kernel_size=3,
                  stride=1, act_layer=nn.ReLU, se_ratio=0.):
         super(GhostBottleneck, self).__init__()
